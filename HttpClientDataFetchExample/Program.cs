15190498c9bbdd7ce3debad9b11e7f8f895c4e8a
TeymurDevv <transmissiyaservis@gmail.com>
2024-01-14 13:21:26 +0400

    Add project files.

---
+++ HttpClientDataFetchExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+static async Task Main()
+    {
+    HttpClient httpClient = new HttpClient();
+    try
+    {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            JArray posts = JArray.Parse(responseBody);
+
+            var filteredPosts = posts.Where(post =>
+                post["id"].ToObject<int>() == 1 ||
+                post["id"].ToObject<int>() == 10 ||
+                post["id"].ToObject<int>() == 100);
+
+            using (StreamWriter file = new StreamWriter("test.txt"))
+            {
+                foreach (var post in filteredPosts)
+                {
+                    await file.WriteLineAsync(post.ToString());
+                }
+            }
+
+            Console.WriteLine("Data has been written to test.txt");
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
